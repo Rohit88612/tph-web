792c44b1d2e36ad295c4fdd8c59334106dcdb469
Rohit88612 <86488569+Rohit88612@users.noreply.github.com>
2025-04-22 23:03:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+// Function to copy either FiveM or Minecraft IP
+function copyIP(serverType) {
+    let serverIP = "";
+    
+    // Set the corresponding server IP based on the type
+    if (serverType === "fivem") {
+      serverIP = "coming soon buddy have some patience"; // Change this to your FiveM IP
+    } else if (serverType === "minecraft") {
+      serverIP = "bhaichrasmp.oops.wtf:25535"; // Change this to your Minecraft IP
+    }
+  
+    // Copy to clipboard and alert the user
+    navigator.clipboard.writeText(serverIP).then(() => {
+      alert(`${serverType.charAt(0).toUpperCase() + serverType.slice(1)} IP copied to clipboard!`);
+    }).catch(err => {
+      console.error("Failed to copy: ", err);
+    });
+  }
+  
